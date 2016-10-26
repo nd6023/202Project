7ae4fd68cd500b3c293524db1b9b636153f6f0e2
kalyanipolagani <kalyani.polagani@sjsu.edu>
2016-10-25 18:34:04 -0700

    Jeopardy World

---
+++ Kalyani/Jeopardy_New/OverScreen.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class OverScreen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class OverScreen extends Actor
+{
+    /**
+     * Act - do whatever the OverScreen wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
