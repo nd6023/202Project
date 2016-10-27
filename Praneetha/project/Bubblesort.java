608d1e6a17285030e239ef28e682ec9845b1c57c
praneetha1595 <praneethadevireddy1595@gmail.com>
2016-10-26 21:45:36 -0700

    Bubblesort basic structure

---
+++ Praneetha/project/Bubblesort.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Bubblesort here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Bubblesort extends Actor
+{ 
+ Obj1 obj1=new Obj1(); Obj2 obj2=new Obj2(); Obj3 obj3=new Obj3(); Obj4 obj4=new Obj4(); Obj5 obj5=new Obj5(); Obj6 obj6=new Obj6();
+    Obj7 obj7=new Obj7(); Obj8 obj8=new Obj8(); Obj9 obj9=new Obj9(); Obj10 obj10=new Obj10(); Obj11 obj11=new Obj11();
+    /**
+     * Act - do whatever the Bubblesort wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    } 
+     public void getArray()
+    {
+        int [] numbers={obj1.getNumber(),obj2.getNumber(),obj3.getNumber(),obj4.getNumber(),obj5.getNumber(),obj6.getNumber(),obj7.getNumber(),obj8.getNumber(),obj8.getNumber(),obj9.getNumber(),obj10.getNumber(),obj11.getNumber()};
+   
+    }
+}
