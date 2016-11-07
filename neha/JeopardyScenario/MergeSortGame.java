e4c9a26144f2c52b939198365f0a2c666fb349bb
nehaparmarcs <np6.023@gmail.com>
2016-11-06 18:17:12 -0800

    Structure for merge sort

--- neha/JeopardyScenario/MergeSortGame.java
+++ neha/JeopardyScenario/MergeSortGame.java
@@ -12,8 +12,17 @@ public class MergeSortGame extends Game
      * Act - do whatever the MergeSortGame wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+	List<Integer> numList;
+	
+	public MergeSortGame(List )
+	
     public void act() 
     {
-        // Add your action code here.
-    }    
+        
+    }   
+    
+    public next getNext(){
+    	
+    	
+    }
 }
