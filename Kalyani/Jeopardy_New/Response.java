d58a4477871f73241b67d47d2c5883e94a3fb1a4
kalyanipolagani <kalyani.polagani@sjsu.edu>
2016-11-13 12:04:33 -0800

    Response class

--- Kalyani/Jeopardy_New/Response.java
+++ Kalyani/Jeopardy_New/Response.java
@@ -18,12 +18,7 @@ public class Response extends Actor implements ActionListener {
     /** The type of the response. */
     public enum Type { RIGHT, WRONG, SELF }
 
-    /**
-     * Create a new response with required information.
-     *
-     * @param q The Question for this response.
-     * @param t The type of this response.
-     */
+
     public Response(Question q, Type t) {
         setQuestion(question);
         setType(t);
@@ -35,12 +30,127 @@ public class Response extends Actor implements ActionListener {
         wrong.addActionListener(this);
     }
 
-    /**
-     * Check for the end of the response.
-     */
+  
     public void act() {
         if (type != Type.SELF && (Greenfoot.isKeyDown("space")
             || Greenfoot.mouseClicked(this))) {
             world.endQuestion();
         }
     }
+    
+     public void setQuestion(Question newQuestion) {
+        question = newQuestion;
+    }
+
+  
+    public void setType(Type t) {
+        type = t;
+    }
+
+    
+    public void actionPerformed(GUIComponent c) {
+        int id = c.getID();
+        if (id == 0) {
+            world.answerSelf(false);
+        } else if (id == 1) {
+            world.answerSelf(true);
+        }
+    }
+
+ 
+    public void clear() {
+        GreenfootImage image = getImage();
+        image.setColor(Color.BLUE);
+        image.fill();
+        if (type == Type.SELF) {
+            world.removeObject(right);
+            world.removeObject(wrong);
+        }
+        world.removeObject(this);
+    }
+
+ 
+    public void addedToWorld(World w) {
+        world = (GameManager) w;
+        draw();
+    }
+
+  
+    private void draw() {
+        final int BORDER = 30;
+        String answer = question.getFirstCorrectAnswerText();
+        String[] explanation =
+            GameManager.wordWrap(question.getExplanation(), LINE_LENGTH);
+        int amount = question.getValue();
+        if (img == null) {
+            World w = getWorld();
+            img = new GreenfootImage(w.getWidth(), w.getHeight());
+        }
+        img.setColor(Color.BLUE);
+        img.fill();
+        img.setColor(Color.WHITE);
+
+        String text = "";
+        if (type == Type.RIGHT) {
+            text = "Congratulations!";
+        } else if (type == Type.WRONG) {
+            text = "Sorry!";
+        }
+        int y = printCentered(text, BORDER + 10, GameManager.LARGE_FONT);
+        y = printCentered("The correct question is...", y,
+            GameManager.MED_FONT);
+        if (answer != null) {
+            text = "\"What is " + answer + "\"";
+        } else {
+            text = "unknown (no answer prepared)";
+        }
+        y = printCentered(text, y, GameManager.MED_FONT);
+        if (type != Type.SELF) {
+            text = "You ";
+            if (type == Type.RIGHT) {
+                text += "won";
+            } else {
+                text += "lost";
+            }
+            text += " " + amount;
+            y = printCentered(text, y, GameManager.MED_FONT);
+        }
+
+        int x = BORDER;
+        y += 10;
+        img.setFont(GameManager.SMALL_FONT);
+        Graphics g = img.getAwtImage().getGraphics();
+        g.setFont(GameManager.SMALL_FONT);
+        FontMetrics fm = g.getFontMetrics();
+        int height = Math.round(fm.getHeight());
+        for (int i = 0; i < explanation.length; i++) {
+            img.drawString(explanation[i], x, y);
+            y += height;
+        }
+
+        if (type != Type.SELF) {
+            img.drawString("Press space or click to continue..." , x,
+                img.getHeight() - BORDER);
+        } else {
+            img.drawString("My answer was: " , x, y + 30);
+            World w = getWorld();
+            w.addObject(right, x + 190, y + 23);
+            w.addObject(wrong, x + 270, y + 23);
+        }
+
+        setImage(img);
+    }
+
+   
+    private int printCentered(String line, int y, Font font) {
+        final float LINE_HEIGHT_MULT = 1.15f;
+        img.setFont(font);
+        Graphics g = img.getAwtImage().getGraphics();
+        g.setFont(font);
+        FontMetrics fm = g.getFontMetrics();
+        int x = img.getWidth() / 2 - fm.stringWidth(line) / 2;
+        img.drawString(line, x, y);
+        y += Math.round(fm.getHeight() * LINE_HEIGHT_MULT);
+        return y;
+    }
+}
