972ec1ab93609f2ede17bebf6e83d573b9411975
navdeepdahiya <nd6.023@gmail.com>
2016-11-19 20:53:10 -0800

    Added new states as per the requirement of the game using state pattern

---
+++ neha/JeopardyScenario/Learning.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Learning here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Learning extends IPlayerState
+{
+    /**
+     * Act - do whatever the Learning wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+    
+    public String getDescription(){
+        //Player is Playing State
+        return "LearningState";
+    }
+}
